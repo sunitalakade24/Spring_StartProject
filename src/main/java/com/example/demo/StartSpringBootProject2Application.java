15eb8d7b9d71998f180026ef4e7fb18af321b627
appasaheb4 <onlyappasaheb4@gmail.com>
2023-10-29 12:29:37 +0530

    SpringStart Project

---
+++ src/main/java/com/example/demo/StartSpringBootProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartSpringBootProject2Application.class, args);
+	}
+
+}
